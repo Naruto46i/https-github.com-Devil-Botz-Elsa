1cc0aaffaeb2e0a5cdac04324f4df9e64555b5de
Naruto46i <150582863+Naruto46i@users.noreply.github.com>
2024-01-02 16:52:22 +0530

    Update openai.py

--- openai.py
+++ openai.py
@@ -1,7 +1,7 @@
 from info import SUPPORT_CHAT_ID, SUPPORT_LINK, OPENAI_API
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from openai import OpenAI 
+from openai import openai 
 
 ai_client = OpenAI(api_key=OPENAI_API)
 
