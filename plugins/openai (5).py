2b451e92da0e1e40002d2b52db5b9d52de24c8d8
Naruto46i <150582863+Naruto46i@users.noreply.github.com>
2024-01-09 16:47:50 +0530

    Update openai (5).py

--- plugins/openai (5).py
+++ plugins/openai (5).py
@@ -1,3 +1,4 @@
+import openai
 from info import SUPPORT_GROUP, SUPPORT_LINK, OPENAI_API
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
